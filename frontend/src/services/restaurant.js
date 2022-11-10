222357d6731fd175e6281aed80f9b7115db123ee
Geordie Ayers <55464996+GeordieAyers@users.noreply.github.com>
2022-11-09 19:22:12 -0500

    front end user functionality with react and bootstrap

---
+++ frontend/src/services/restaurant.js
@@ -0,0 +1,41 @@
+import http from "../http-common"; // import the AXIOS base URL
+
+class RestaurantDataService {
+ // write different methods that serve as individual api calls that go through axios
+
+// GET requests -> action that gets data based on a query with filters
+// they work by taking the base url and then returning a custom url based on parameters
+    getAll(page = 0) {
+        return http.get(`restaurants?page=${page}`); // get all restaurants at page 0 default
+    }
+
+    get(id) {
+        return http.get(`/restaurant?id=${id}`); // get a single restaurant of an ID
+    }
+
+    find(query, by = "name", page = 0) {
+        return http.get(`restaurants?${by}=${query}&page=${page}`); // find restaurant
+    } 
+
+    getCuisines(id) {
+        return http.get(`/cuisines`); // get cuisines
+      }
+
+// POST -> adding a child resource
+    createReview(data) {
+        return http.post("/review-new", data); //post new review
+    }
+
+// PUT -> modify single resource
+    updateReview(data) {
+        return http.put("/review-edit", data); /// update existign review
+    }
+
+// DELETE -> remove data
+    deleteReview(id, userId) {
+        return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}}); // delete review by id and user
+    }
+
+}
+
+export default new RestaurantDataService();
